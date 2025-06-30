5d5427e9bee49295b2788103e902648f3dc94fcc
priyankarumane08 <priyankaumane86@gmail.com>
2025-06-30 12:59:02 +0530

    Basic Structure of icici

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pipe1Pipe } from './shared/pipe1.pipe';
+import { Directive1Directive } from './shared/directive1.directive';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pipe1Pipe,
+    Directive1Directive,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
